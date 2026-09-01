83f3886a685ba4f80ffecbbbce9c45a2b321c623
vaxzzzz <victorvaz06@gmail.com.com>
2026-09-01 20:28:44 -0300

    relogio digital

---
+++ relogio-digital/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './DigitalClock.css'; // Importe o arquivo CSS aqui
+
+export function DigitalClock() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  return (
+    <div className="clock-container">
+      <span className="clock-display">{time}</span>
+    </div>
+  );
+}
+
+export default DigitalClock;
